890fc089baf7dcaba2f35ac813ecb685ec3d1df0
Pritesh Kamble <priteshkamble13@gmail.com>
2026-03-11 19:17:14 +0530

    three stages are added

--- FlightReservationApplication/backend.groovy
+++ FlightReservationApplication/backend.groovy
@@ -4,11 +4,26 @@ pipeline{
         stage('Code-pull'){
             steps{
 
-                git branch: 'main', url: 'https://github.com/PriteshK13/FlightReservation-p1.git'
-        
-                
+               git branch: 'main', url: 'https://github.com/PriteshK13/FlightReservation-p1.git'
             }
         }
-       
-    }
+         stage('Build'){
+            steps{
+                sh '''
+                    cd FlightReservationApplication
+                    mvn clean package
+                '''
+            }
+        }
+         stage('QA-Test'){
+            steps{
+                withSonarQubeEnv(installationName: 'sonar', credentialsId: 'sonar-token') {
+                  sh'''
+                     cd FlightReservationApplication
+                     mvn sonar:sonar -Dsonar.projectKey=flight-reservation   
+                  '''
+                }
+            }
+        }
+    } 
 }
