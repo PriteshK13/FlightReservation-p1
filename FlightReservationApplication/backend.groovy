e89e226c8fcb569b50557ab2d9f55f645621e910
Pritesh Kamble <priteshkamble13@gmail.com>
2026-04-23 17:08:39 +0530

    sonar-qube

--- FlightReservationApplication/backend.groovy
+++ FlightReservationApplication/backend.groovy
@@ -6,5 +6,24 @@ pipeline {
                 git branch: 'main', url: 'https://github.com/PriteshK13/FlightReservation-p1.git'
             }
         }
+        stage('code-build') {
+            steps{
+                '''
+                cd FlightReservationApplication
+                mvn clean package
+                '''
+            }
+        }
+        stage('QA-Test') {
+            steps{
+                withSonarQubeEnv(instalationName:'sonar' credentialsId: 'Sonar-token') {
+                    sh '''
+                    cd FlightReservationApplication
+                    mvn sonar:sonar -Dsonar.projectkey=flight-reservation
+                
+                    '''
+                }
+            }
+        }
     }
 }
